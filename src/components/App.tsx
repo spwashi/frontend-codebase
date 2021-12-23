6aea456a4b7e0fe54add0ffa8e218eaeeb71c2ab
Sam Washington <sam@spwashi.com>
2021-12-22 22:12:44 -0600

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import React from 'react';
+
+import { SignUpForm } from './user/SignUpForm/SignUpForm'
+
+function App() {
+    return ( <div className = "App" >
+        <SignUpForm></SignUpForm>
+         </div>
+    );
+}
+
+export default App;
