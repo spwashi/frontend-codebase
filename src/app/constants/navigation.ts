10a09004616c3babb3fc24c9b724170e46aaed00
Sam Washington <sam@spwashi.com>
2022-01-21 13:15:39 -0600

    updated navigation

--- src/app/constants/navigation.ts
+++ src/app/constants/navigation.ts
@@ -1,4 +1,30 @@
 export const navLinks = [
     {href: '/', title: 'Home'},
-
+    {href: '/museum', title: 'Museum'},
+    {
+        href:     '/improv',
+        title:    'Improv',
+        children: [
+            {href: '/improv/shows', title: 'Improv Shows'},
+            {href: '/improv/classes', title: 'Improv Classes'},
+        ],
+    },
+    {
+        href:     '/stand-up',
+        title:    'Stand-up',
+        children: [
+            {href: '/stand-up/shows', title: 'Stand-up Shows'},
+            {href: '/stand-up/classes', title: 'Stand-up Classes'},
+        ],
+    },
+    {href: '/team', title: 'Meet the team'},
+    {
+        href:     '/events',
+        title:    'Events',
+        children: [
+            {href: '/events/book', title: 'Parties'},
+            {href: '/events/calendar', title: 'Calendar'},
+            {href: '/events/list', title: 'Schedule'},
+        ],
+    },
 ]
