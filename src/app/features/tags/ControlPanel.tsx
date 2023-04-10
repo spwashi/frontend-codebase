704d9da6bdc95ffbc3d0e237e61c5ba703b4735e
Sam Washington <sam@spwashi.com>
2023-04-10 06:35:32 -0500

    continued normalizing forms

--- src/app/features/tags/ControlPanel.tsx
+++ src/app/features/tags/ControlPanel.tsx
@@ -5,22 +5,34 @@ import {RestrictedCreateTagForm} from './behaviors/create';
 import {TagsDisplayForm} from './components/display';
 import {UserFeatures} from '../users/Features';
 import {ProjectFeatures} from '../projects/Features';
+import {NavLink} from 'react-router-dom';
+import {Route, Routes} from 'react-router';
 
 export function TagsControlPanel() {
-    return (
-        <>
-            <TagFeatures/>
-            <UserFeatures/>
-            <ProjectFeatures/>
-
-            <FeatureRequirement name="tags" alternative={'Need Tags'}>
-                <section>
-                    <RestrictedCreateTagForm/>
-                    <FeatureRequirement name="tags.display" alternative={'Need Tag Display'}>
-                        <TagsDisplayForm/>
-                    </FeatureRequirement>
-                </section>
-            </FeatureRequirement>
-        </>
-    );
+  return (
+    <>
+      <TagFeatures/>
+      <UserFeatures/>
+      <ProjectFeatures/>
+      <nav>
+        <ul>
+          <li><NavLink to="../tags">Tag Home</NavLink></li>
+          <li><NavLink to="../tags/all">All Tags</NavLink></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="all" element={"[unimplemented]"}/>
+        <Route path="" element={
+          <FeatureRequirement name="tags" alternative={'Need Tags'}>
+            <section>
+              <RestrictedCreateTagForm/>
+              <FeatureRequirement name="tags.display" alternative={'Need Tag Display'}>
+                <TagsDisplayForm/>
+              </FeatureRequirement>
+            </section>
+          </FeatureRequirement>
+        }/>
+      </Routes>
+    </>
+  );
 }
