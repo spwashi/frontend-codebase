1d94c83996e7e3a3794e0e93aa4e6efdd2b40ee4
Sam Washington <sam@spwashi.com>
2023-04-13 01:24:16 -0500

    moved useChangeEffect

--- src/app/_features/projects/_services/.graphql/one/index.tsx
+++ src/app/_features/projects/_services/.graphql/one/index.tsx
@@ -6,7 +6,7 @@ import {OneProjectQuery} from './components/Query';
 import {ProjectContext} from '../../../context/context';
 import {useDispatch} from 'react-redux';
 import {ACTION_PROJECT_SELECTED} from '../../.redux/reducer';
-import {useChangeEffect} from '../../../../../_services/storage/localStorage/hooks/useChangeEffect';
+import {useChangeEffect} from '../../../../../hooks/util/useChangeEffect';
 
 function SiteProjectDeactivateButton() {
   const context = useContext(ProjectContext);
--- src/app/_services/features/hooks/useFeatureQuery.ts
+++ src/app/_services/features/hooks/useFeatureQuery.ts
@@ -1,5 +1,5 @@
 import {DocumentNode, useApolloClient, useQuery} from '@apollo/client';
-import {useChangeEffect} from '../../storage/localStorage/hooks/useChangeEffect';
+import {useChangeEffect} from '../../../hooks/util/useChangeEffect';
 import {useDispatch} from 'react-redux';
 import {useEffect} from 'react';
 import {ACTION_GRAPHQL, ACTION_NOGRAPHQL} from '../../redux/reducer';
