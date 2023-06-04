3731721200401270bc982a74a6677b30c4c410ca
Sam Washington <sam@spwashi.com>
2023-06-04 13:10:09 -0500

    added e2e stubs for the other entity types

--- cypress/e2e/features/admin/adminFeatures.cy.ts
+++ cypress/e2e/features/admin/adminFeatures.cy.ts
@@ -10,4 +10,40 @@ describe("Admin Features Are Grounded", () => {
       "be.visible"
     );
   });
+  it("Has a link to Concepts; Concept Features Are Not Enabled", () => {
+    cy.get(".admin--nav").contains("a", "Concepts").click();
+    cy.contains("[this feature is not enabled: app--concepts]").should(
+      "be.visible"
+    );
+  });
+  it("Has a link to Events; Event Features Are Not Enabled", () => {
+    cy.get(".admin--nav").contains("a", "Events").click();
+    cy.contains("[this feature is not enabled: app--events]").should(
+      "be.visible"
+    );
+  });
+  it("Has a link to Projects; Project Features Are Not Enabled", () => {
+    cy.get(".admin--nav").contains("a", "Projects").click();
+    cy.contains("[this feature is not enabled: app--projects]").should(
+      "be.visible"
+    );
+  });
+  it("Has a link to Scenes; Scene Features Are Not Enabled", () => {
+    cy.get(".admin--nav").contains("a", "Scenes").click();
+    cy.contains("[this feature is not enabled: app--scenes]").should(
+      "be.visible"
+    );
+  });
+  it("Has a link to Tags; Tag Features Are Not Enabled", () => {
+    cy.get(".admin--nav").contains("a", "Tags").click();
+    cy.contains("[this feature is not enabled: app--tags]").should(
+      "be.visible"
+    );
+  });
+  it("Has a link to Users; User Features Are Not Enabled", () => {
+    cy.get(".admin--nav").contains("a", "Users").click();
+    cy.contains("[this feature is not enabled: app--users]").should(
+      "be.visible"
+    );
+  });
 });
