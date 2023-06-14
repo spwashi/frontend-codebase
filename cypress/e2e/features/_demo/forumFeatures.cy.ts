ff0b9e1d4ed4d7f1d6ac33e5c40ac94729c5a3ba
Sam Washington <sam@spwashi.com>
2023-06-14 18:02:06 -0500

    Forum Example: renamed test

--- cypress/e2e/features/_demo/forumFeatures.cy.ts
+++ cypress/e2e/features/_demo/forumFeatures.cy.ts
@@ -1,9 +1,10 @@
 import { forumRoutes, getAbsoluteUrl } from "../../../__generated__/routes";
 
-describe("Forum Features Are Grounded", () => {
+describe("Forum Features", () => {
   beforeEach(() => {
     cy.visit(getAbsoluteUrl(forumRoutes.root));
   });
+
   it("Has a link to Posts", () => {
     cy.contains("a", "Posts");
   });
