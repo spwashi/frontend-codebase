3126b7fa15e870456184bda0781708816ef94467
Sam Washington <sam@spwashi.com>
2023-06-15 21:23:08 -0500

    normalized Forum Feature Cypress Tests

--- cypress/e2e/features/_demo/forumFeatures.cy.ts
+++ cypress/e2e/features/_demo/forumFeatures.cy.ts
@@ -1,8 +1,7 @@
 import { forumRoutes, getAbsoluteUrl } from "../../../__generated__/routes";
 
 function clickFirstPost() {
-  cy.contains("a", "Posts").first().click();
-  cy.location("pathname").should("include", "forum/posts");
+  navigateToPostsPageViaNavbar();
   cy.get(".forum-post a").first().click();
 }
 function clickFirstCommentPermalink() {
@@ -11,6 +10,26 @@ function clickFirstCommentPermalink() {
 function navigateToUsersPage() {
   cy.contains("a", "Users").first().click();
   cy.location("pathname").should("include", "forum/users");
+}
+function clickLoginLink() {
+  cy.contains("a", "Log In").first().click();
+}
+function clickLogoutLink() {
+  cy.contains("a", "Log Out").first().click();
+}
+function clickSessionStatusLink() {
+  cy.contains("a", "Status").first().click();
+}
+function navigateToPostsPageViaNavbar() {
+  cy.contains("a", "Posts").first().click();
+  cy.location("pathname").should("include", "forum/posts");
+}
+function navigateToCommentsPageViaNavbar() {
+  cy.contains("a", "Comments").first().click();
+  cy.location("pathname").should("include", "forum/comments");
+}
+function clickFirstLinkInUserElement() {
+  cy.get(".forum-user a").first().click();
 }
 describe("Forum Features", () => {
   beforeEach(() => {
@@ -21,17 +40,15 @@ describe("Forum Features", () => {
     cy.get("section[data-feature-name=app--forum]");
   });
   it("Has a Page for a List of Posts", () => {
-    cy.contains("a", "Posts").first().click();
-    cy.location("pathname").should("include", "forum/posts");
+    navigateToPostsPageViaNavbar();
     cy.get("section[data-feature-name=app--forum--posts]");
   });
   it("Has a Page for Specific Posts", () => {
     clickFirstPost();
     cy.get("section[data-feature-name=app--forum--single-post]");
   });
-  it("Has Comment Page", () => {
-    cy.contains("a", "Comments").first().click();
-    cy.location("pathname").should("include", "forum/comments");
+  it("Has a Page for Comments", () => {
+    navigateToCommentsPageViaNavbar();
     cy.get("section[data-feature-name=app--forum--comments]");
     clickFirstCommentPermalink();
     cy.get("section[data-feature-name=app--forum--comment-route]");
@@ -46,20 +63,20 @@ describe("Forum Features", () => {
     cy.get("section[data-feature-name=app--forum--users]");
   });
   it("Has a Page for Logging In", () => {
-    cy.contains("a", "Log In").first().click();
+    clickLoginLink();
     cy.get("section[data-feature-name=app--forum--login]");
   });
   it("Has a Page for Logging Out", () => {
-    cy.contains("a", "Log Out").first().click();
+    clickLogoutLink();
     cy.get("section[data-feature-name=app--forum--logout]");
   });
   it("Has a Page to Check Session Status", () => {
-    cy.contains("a", "Status").first().click();
+    clickSessionStatusLink();
     cy.get("section[data-feature-name=app--forum--session-status]");
   });
   it("Has a Page for User Profiles", () => {
     navigateToUsersPage();
-    cy.get(".forum-user a").first().click();
+    clickFirstLinkInUserElement();
     cy.get("section[data-feature-name=app--forum--user-profile]");
   });
 });
